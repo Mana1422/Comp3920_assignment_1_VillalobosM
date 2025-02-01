eb4825adb0e11534cd9f9efe0c6a3f9df2d79925
Mana1422 <manasesvillalobos80@gmail.com>
2025-02-01 14:11:29 -0800

    Started working with ejs and made a simple login form

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 const express = require('express');
 require('dotenv').config();
 
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
 
-// app.use(session({
-//     secret: node_session_secret,
-//     saveUninitialized: false,
-//     resave: true
-// }));
+// To render ejs
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+app.use(session({
+    secret: node_session_secret,
+    saveUninitialized: false,
+    resave: true
+}));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render("home");
+});
+
+app.get('/login', (req, res)=> {
+    res.render("login")
+});
+
+app.post('/verifyLogin', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    res.render("home", {email, password});
+});
+
+app.get('/about', (req,res) => {
+	const color = req.query.color;
+	
+	res.send("<h1 style='color:" + color +";'>Manases Villalobos</h1>")
 });
 
 app.get('/home', (req, res) =>{
     res.send('Welcome! Come in please')
 });
 
-app.get('/*', (req, res) => {
-    res.send('404 Page not found')
+// for images 
+app.use(express.static(__dirname + "/public"))
+
+app.get('*', (req, res) => {
     res.status(404);
+    res.send('Page not found - 404')
 });
 
 app.listen(port, () =>{
